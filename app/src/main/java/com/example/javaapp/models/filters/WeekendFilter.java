fac5ac4404c9b72b586deaa411e3ab7d0a330b8f
Markus Kollotzek <markus.kollotzek@posteo.de>
2019-11-28 18:17:40 +0100

    Fix weekend bug

--- app/src/main/java/com/example/javaapp/models/filters/WeekendFilter.java
+++ app/src/main/java/com/example/javaapp/models/filters/WeekendFilter.java
@@ -2,29 +2,42 @@ package com.example.javaapp.models.filters;
 
 import com.example.javaapp.models.Event;
 
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import static java.time.temporal.TemporalAdjusters.next;
+import static java.util.Calendar.MONDAY;
+import static java.util.Calendar.SUNDAY;
 
 public class WeekendFilter implements FilterStrategy {
 
-    private final List<Integer> WEEKEND_DAYS = Arrays.asList(Calendar.FRIDAY, Calendar.SATURDAY, Calendar.SUNDAY);
+    private final List<Integer> WEEKEND_DAYS = Arrays.asList(Calendar.FRIDAY, Calendar.SATURDAY, SUNDAY);
 
     @Override
     public void applyFilter(List<Event> input, List<Event> output) {
-        Calendar nextWeek = Calendar.getInstance();
-        nextWeek.add(Calendar.DAY_OF_MONTH, 7);
-
-        Calendar c = Calendar.getInstance();
-
-        output.clear();
+        Calendar forCalendar = Calendar.getInstance();
+        Calendar nextMonday = nextMonday();
 
         for (Event e : input) {
-            c.setTime(e.date);
+            forCalendar.setTime(e.date);
 
-            if (c.before(nextWeek) && WEEKEND_DAYS.contains(c.get(Calendar.DAY_OF_WEEK))) {
+            if (forCalendar.before(nextMonday) && WEEKEND_DAYS.contains(forCalendar.get(Calendar.DAY_OF_WEEK))) {
                 output.add(e);
             }
         }
+    }
+
+    private Calendar nextMonday() {
+        Calendar c = Calendar.getInstance();
+
+        do {
+            c.add(Calendar.DAY_OF_WEEK, 1);
+        } while (c.get(Calendar.DAY_OF_WEEK) != MONDAY);
+
+        return c;
     }
 }
